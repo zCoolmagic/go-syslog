f1522c686f939c29d4c8b8f73d08ade70d1fba44
zCoolmagic <44434466+zCoolmagic@users.noreply.github.com>
2020-07-29 13:16:34 +0300

    Update rfc5424.go

--- internal/syslogparser/rfc5424/rfc5424.go
+++ internal/syslogparser/rfc5424/rfc5424.go
@@ -243,8 +243,10 @@ func (p *Parser) parseHostname() (string, error) {
 }
 
 // APP-NAME = NILVALUE / 1*48PRINTUSASCII
+// 2020-07-28 zCoolmagic change length application name for Docker tags
+// incompability features
 func (p *Parser) parseAppName() (string, error) {
-	return parseUpToLen(p.buff, &p.cursor, p.l, 48, ErrInvalidAppName)
+	return parseUpToLen(p.buff, &p.cursor, p.l, 255, ErrInvalidAppName)
 }
 
 // PROCID = NILVALUE / 1*128PRINTUSASCII
